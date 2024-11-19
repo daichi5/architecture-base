ce16b19e261c6bca60b93413e6d1d453ad546973
daichi5 <sawa22daichi@gmail.com>
2024-11-19 23:38:20 +0900

    propagate context

--- go/internal/presentation/handlers/user_handler.go
+++ go/internal/presentation/handlers/user_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"api/internal/domain/models"
 	"api/internal/domain/repositories"
+	"context"
 )
 
 type UserHandler struct {
@@ -13,8 +14,10 @@ func NewUserHandler(repository repositories.UserRepository) *UserHandler {
 	return &UserHandler{userRepository: repository}
 }
 
-func (h *UserHandler) FindAll() []models.User {
-	users, _ := h.userRepository.FindAll()
+func (h *UserHandler) FindAll(ctx context.Context) []models.User {
+	users, _ := h.userRepository.FindAll(ctx)
+
+	// TODO: handle error
 
 	return users
 }
--- go/internal/presentation/router/router.go
+++ go/internal/presentation/router/router.go
@@ -1,20 +1,31 @@
 package router
 
 import (
+	"api/ent"
 	"api/internal/infrastructure/repositories"
 	"api/internal/presentation/handlers"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.Default()
 
-	userRepository := handlers.NewUserHandler(&repositories.UserRepositoryImpl{})
+	client, err := ent.Open("postgres", "host=db port=5432 user=postgres dbname=dbname password=password sslmode=disable")
+
+	if err != nil {
+		panic(err)
+	}
+
+	userRepository := repositories.NewUserRepository(client)
+	newUserHandler := handlers.NewUserHandler(userRepository)
 	r.GET("/users", func(c *gin.Context) {
-		users := userRepository.FindAll()
-		c.JSON(http.StatusOK, users)
+		users := newUserHandler.FindAll(c)
+
+		c.JSON(http.StatusOK, gin.H{"users": users})
 	})
+
 	return r
 }
