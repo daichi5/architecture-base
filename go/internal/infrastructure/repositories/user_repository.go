5e636aa690b8c194defd355c4973db20e8b86ab1
daichi5 <sawa22daichi@gmail.com>
2024-11-19 23:38:02 +0900

    implement userRepository impl data access

--- go/internal/infrastructure/repositories/user_repository.go
+++ go/internal/infrastructure/repositories/user_repository.go
@@ -1,18 +1,44 @@
 package repositories
 
 import (
+	"api/ent"
 	"api/internal/domain/models"
 	"api/internal/domain/repositories"
+	"context"
+	"fmt"
 )
 
-type UserRepositoryImpl struct{}
+type UserRepositoryImpl struct {
+	db *ent.Client
+}
 
 // check interface at compile time
 var _ repositories.UserRepository = (*UserRepositoryImpl)(nil)
 
-func (r *UserRepositoryImpl) FindAll() ([]models.User, error) {
-	return []models.User{
-		{ID: 1, Name: "John"},
-		{ID: 2, Name: "Doe"},
-	}, nil
+func NewUserRepository(db *ent.Client) *UserRepositoryImpl {
+	return &UserRepositoryImpl{db: db}
+}
+
+func (r *UserRepositoryImpl) FindAll(ctx context.Context) ([]models.User, error) {
+	u, err := r.db.User.Query().All(ctx)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, fmt.Errorf("failed to find users: %w", err)
+	}
+
+	users := make([]models.User, len(u), len(u))
+
+	for i, user := range u {
+		users[i] = *toUserModel(user)
+	}
+
+	return users, nil
+}
+
+func toUserModel(u *ent.User) *models.User {
+	return &models.User{
+		ID:   u.ID,
+		Name: u.Name,
+	}
 }
