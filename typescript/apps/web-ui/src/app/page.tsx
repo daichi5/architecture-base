73b6d316e2d66f2f44e6ffa93e01bb46e9dfc45b
daichi5 <sawa22daichi@gmail.com>
2024-12-02 23:43:23 +0900

    add Container layout

--- typescript/apps/web-ui/src/app/page.tsx
+++ typescript/apps/web-ui/src/app/page.tsx
@@ -2,16 +2,19 @@ import { Button } from "@/components/foundations/Button";
 import { Cluster } from "@/components/foundations/Cluster";
 import { Stack } from "@/components/foundations/Stack";
 import { Header } from "@/components/layout/Header";
+import { Container } from "@/components/foundations/Container";
 
 export default function Home() {
 	return (
 		<Stack>
 			<Header />
-			<h1>Home</h1>
-			<Cluster>
-				<Button>Click me!</Button>
-				<Button variant="destructive">Click me!</Button>
-			</Cluster>
+			<Container maxWidth="1200px">
+				<h1>Home</h1>
+				<Cluster>
+					<Button>Click me!</Button>
+					<Button variant="destructive">Click me!</Button>
+				</Cluster>
+			</Container>
 		</Stack>
 	);
 }
--- typescript/apps/web-ui/src/components/layout/Header/index.tsx
+++ typescript/apps/web-ui/src/components/layout/Header/index.tsx
@@ -1,18 +1,25 @@
 import { Link } from "@/components/foundations/Link";
 import { Cluster } from "@/components/foundations/Cluster";
+import { Container } from "@/components/foundations/Container";
+import { Button } from "@/components/foundations/Button";
 
 export const Header: React.FC = () => {
 	return (
-		<header className="p-4 bg-gray-800 text-white">
-			<Cluster gap="16px">
-				<h1>Your name</h1>
-				<nav>
+		<header className="p-4  text-white bg-neutral-950 border-b-neutral-600 border-b-2">
+			<Container maxWidth="1200px">
+				<Cluster align="center" justify="space-between">
 					<Cluster gap="16px">
-						<Link href="/">Home</Link>
-						<Link href="/users">Users</Link>
+						<h1>Your name</h1>
+						<nav>
+							<Cluster gap="16px">
+								<Link href="/">Home</Link>
+								<Link href="/users">Users</Link>
+							</Cluster>
+						</nav>
 					</Cluster>
-				</nav>
-			</Cluster>
+					<Button>Sign in</Button>
+				</Cluster>
+			</Container>
 		</header>
 	);
 };
