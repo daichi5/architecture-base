20bb3b1c572f84d8f1a7beba0168d20e8c9779c5
daichi5 <sawa22daichi@gmail.com>
2024-12-02 23:42:44 +0900

    add align, justify props

--- typescript/apps/web-ui/src/components/foundations/Cluster/index.tsx
+++ typescript/apps/web-ui/src/components/foundations/Cluster/index.tsx
@@ -1,14 +1,25 @@
+import type { CSSProperties } from "react";
+
 type Gap = "4px" | "8px" | "16px";
 
 type Props = {
 	children: React.ReactNode;
 	gap?: Gap;
+	align?: CSSProperties["alignItems"];
+	justify?: CSSProperties["justifyContent"];
 };
 
-const Cluster: React.FC<Props> = ({ gap = "4px", children }) => {
+const Cluster: React.FC<Props> = (props) => {
 	return (
-		<div className="flex flex-row" style={{ gap: gap }}>
-			{children}
+		<div
+			className="flex flex-row"
+			style={{
+				gap: props.gap,
+				justifyContent: props.justify,
+				alignItems: props.align,
+			}}
+		>
+			{props.children}
 		</div>
 	);
 };
