52aeb85257c3a95d22777d0d6eb990defde35bb5
daichi5 <sawa22daichi@gmail.com>
2024-12-03 23:32:06 +0900

    add Table component

--- typescript/apps/web-ui/src/app/page.tsx
+++ typescript/apps/web-ui/src/app/page.tsx
@@ -3,17 +3,45 @@ import { Cluster } from "@/components/foundations/Cluster";
 import { Stack } from "@/components/foundations/Stack";
 import { Header } from "@/components/layout/Header";
 import { Container } from "@/components/foundations/Container";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableCaption,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/foundations/Table";
 
 export default function Home() {
 	return (
-		<Stack>
+		<Stack gap="8px">
 			<Header />
 			<Container maxWidth="1200px">
-				<h1>Home</h1>
-				<Cluster>
-					<Button>Click me!</Button>
-					<Button variant="destructive">Click me!</Button>
-				</Cluster>
+				<Stack gap="8px">
+					<h1 className="text-4xl font-bold text-stone-400">Users</h1>
+					<Table>
+						<TableCaption>A list of users.</TableCaption>
+						<TableHeader>
+							<TableRow>
+								<TableHead className="w-[100px]">userId</TableHead>
+								<TableHead>userName</TableHead>
+								<TableHead>email</TableHead>
+								<TableHead className="text-right">createdAt</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							<TableRow>
+								<TableCell className="font-medium">1</TableCell>
+								<TableCell>John Doe</TableCell>
+								<TableCell>sample@xxxxx</TableCell>
+								<TableCell className="text-right">
+									2021-10-10 10:10:10
+								</TableCell>
+							</TableRow>
+						</TableBody>
+					</Table>
+				</Stack>
 			</Container>
 		</Stack>
 	);
