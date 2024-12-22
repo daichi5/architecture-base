d3bde9f55b4a9c0d3e0e7fc99bfdb7952cc250af
daichi5 <sawa22daichi@gmail.com>
2024-12-23 03:15:52 +0900

    add align property

--- typescript/apps/web-ui/src/components/foundations/Stack/index.tsx
+++ typescript/apps/web-ui/src/components/foundations/Stack/index.tsx
@@ -1,14 +1,20 @@
+import type { CSSProperties } from "react";
+
 type Gap = "4px" | "8px" | "16px" | "32px";
 
 type Props = {
 	children: React.ReactNode;
 	gap?: Gap;
+	align?: CSSProperties["alignItems"];
 };
 
-const Stack: React.FC<Props> = ({ gap = "4px", children }) => {
+const Stack: React.FC<Props> = (props) => {
 	return (
-		<div className="flex flex-col" style={{ gap: gap }}>
-			{children}
+		<div
+			className="flex flex-col"
+			style={{ gap: props.gap, alignItems: props.align || "stretch" }}
+		>
+			{props.children}
 		</div>
 	);
 };
